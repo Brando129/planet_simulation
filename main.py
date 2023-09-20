8a616223c1f561f6e802bda5b4e672a5650e8d81
Brandon Colangelo <brandoncancode@gmail.com>
2023-09-20 10:43:13 -0400

    Built planet class, made a sun instance.

--- main.py
+++ main.py
@@ -7,10 +7,47 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
 WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+
+class Planet:
+    # Class variables
+    # Astronomical unit.
+    AU = 149.6e6 * 1000
+    # Gravitational constant.
+    G = 6.67428e-11
+    SCALE = 250 / AU # 1 AU = 100 pixels
+    TIMESTEP = 3600*24 # 1 day
+
+    # Planet initialization
+    def __init__(self, x, y, radius, color, mass):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.mass = mass
+
+        self.orbit = []
+        self.sun = False
+        self.distance_to_sun = 0
+
+        self.x_vel = 0
+        self.y_vel = 0
+
+    def draw(self, win):
+        x = self.x * self.SCALE + WIDTH / 2
+        y = self.y * self.SCALE + HEIGHT / 2
+        pygame.draw.circle(win, self.color, (x, y), self.radius)
+
+
 
 def main():
     run = True
     clock = pygame.time.Clock()
+
+    sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10**30)
+    sun.sun = True
+
+    planets = [sun]
 
     while run:
         clock.tick(60)
@@ -20,6 +57,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        for planet in planets:
+            planet.draw(WIN)
+        pygame.display.update()
     pygame.quit()
 
 main()
